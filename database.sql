e21db06bc167a09cb4fbe9e01adf1bc756bd0944
Hung Nguyen <tinapc@gmail.com>
2016-05-10 15:40:45 +0700

    No message

---
+++ database.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate - MySQL GUI v8.21 
+MySQL - 5.6.20 : Database - calendar
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`calendar` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `calendar`;
+
+/*Table structure for table `events` */
+
+DROP TABLE IF EXISTS `events`;
+
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `desc` text,
+  `start` text,
+  `end` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+/*Data for the table `events` */
+
+insert  into `events`(`id`,`title`,`desc`,`start`,`end`) values (1,'Event 1',NULL,'2016-05-10','2016-05-14'),(4,'Event 4',NULL,'2016-05-06','2016-05-07');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
